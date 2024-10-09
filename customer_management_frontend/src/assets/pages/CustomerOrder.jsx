603b171553ea3cfa2e86c2a3dc3c7a222a9044a6
JJFelix <felixomwenga441@gmail.com>
2024-10-09 22:13:39 +0300

    Final touches-1

--- customer_management_frontend/src/assets/pages/CustomerOrder.jsx
+++ customer_management_frontend/src/assets/pages/CustomerOrder.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 import { backend_url, dateTime } from '../../App'
 
@@ -13,19 +13,19 @@ const CustomerOrder = () => {
     axios
     .get(`${backend_url}/api/customers/${customer_id}`)
     .then((response)=>{
-      // console.log(response.data)
+      console.log(response.data)
       setCustomer(response.data)      
     })
     .catch((error)=>{
       console.error(error.response.data.error)     
     })
-  })
+  }, [])
 
   console.log("Customer data: ", customer?.customer, customer?.orders)
   
   return (
     <div className='main-wrapper'>
-      <h1>Customer: {customer?.customer.name}</h1>
+      <h1>Customer: {customer.customer?.name}</h1>
       <h2>Orders</h2>
       <div className='data mt-4'>        
         {customer.orders?.map((order, index) => (
@@ -35,8 +35,9 @@ const CustomerOrder = () => {
             <p><strong>Time: </strong>{dateTime(order.time)}</p>
           </div>
         ))}
-        <Link className='btn btn-primary m-1' to={{pathname:`/customers`}}>Back to Customers</Link>
       </div>
+      <Link className='btn btn-primary m-1' to={{pathname:`/customers`}}>Back to Customers</Link>
+
     </div>
   )
 }
