31f8e95a96ceb96ed74224361be3d6556b7abee9
JJFelix <felixomwenga441@gmail.com>
2024-10-09 12:30:42 +0300

    adjusted database settings

--- customer_management_api/customer_management/settings.py
+++ customer_management_api/customer_management/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'customer_management.wsgi.application'
 # }
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv(DATABASE_URL),
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600
     )
 }
